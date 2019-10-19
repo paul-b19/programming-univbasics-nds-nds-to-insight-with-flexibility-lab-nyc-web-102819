fda86e96f8b17766d3ebdfd9231a900a13f7d781
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-19 05:42:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -105,7 +105,7 @@ def movies_with_directors_set(source)
   aoh_movies_with_dir = []
   index = 0 
   while index < source.count do 
-    name = source[index][:director_name]
+    name = source[index][:name]
     movies_collection = source[index][:movies]
     aoh_one_dir = movies_with_director_key(name, movies_collection)
     aoh_movies_with_dir << aoh_one_dir
